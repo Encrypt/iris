dc714442e5e3ef3200e79d97fd41b72ac90167be
Yann Privé <encrypt@encrypt-tips.tk>
2016-05-31 23:02:25 +0100

    Reviewed the database structure to include the DMOZ project.

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 -- Datasets corresponding to the PCAP files added
 CREATE TABLE datasets (
-	id 		SERIAL 			PRIMARY KEY	NOT NULL,
+	id		SERIAL			PRIMARY KEY,
 	name	VARCHAR(255)	NOT NULL,
 	md5sum	CHAR(32)		NOT NULL UNIQUE,
 	added	TIMESTAMP		NOT NULL
@@ -8,20 +8,53 @@ CREATE TABLE datasets (
 
 -- Protocols identified by libprotoident
 CREATE TABLE protocols (
-	id 		SERIAL 		PRIMARY KEY	NOT NULL,
+	id		SERIAL		PRIMARY KEY,
 	name	VARCHAR(50)	NOT NULL UNIQUE
+);
+
+-- Topics of the categories (using DMOZ classification)
+CREATE TABLE topics (
+	id		SERIAL		PRIMARY KEY,
+	name	VARCHAR(50)	NOT NULL UNIQUE
+);
+
+-- Subtopics of the categories (using DMOZ classification)
+CREATE TABLE subtopics (
+	id		SERIAL		PRIMARY KEY,
+	name	VARCHAR(50)	NOT NULL UNIQUE
+);
+
+-- Categories of the websites
+CREATE TABLE categories (
+	id			SERIAL	PRIMARY KEY,
+	topic		INT		NOT NULL REFERENCES topics(id),
+	subtopic	INT		NOT NULL REFERENCES subtopics(id)
+);
+
+-- URLs of the websites
+CREATE TABLE urls (
+	id		SERIAL			PRIMARY KEY,
+	value	VARCHAR(255)	NOT NULL UNIQUE
+);
+
+-- DMOZ database
+CREATE TABLE dmoz (
+	id				SERIAL	PRIMARY KEY,
+	url				INT		NOT NULL UNIQUE REFERENCES urls(id),
+	category		INT		NOT NULL REFERENCES categories(id)
 );
 
 -- Websites visited by the users
 CREATE TABLE websites (
-	id 			SERIAL			PRIMARY KEY	NOT NULL,
-	url			VARCHAR(255)	NOT NULL UNIQUE,
-	category	VARCHAR(20)
+	id				SERIAL	PRIMARY KEY,
+	url				INT		NOT NULL REFERENCES urls(id),
+	category		INT		REFERENCES categories(id),
+	hand_classified	BOOLEAN
 );
 
 -- Flows identified by libprotoident
 CREATE TABLE flows (
-	id 				BIGSERIAL	PRIMARY KEY,
+	id				BIGSERIAL	PRIMARY KEY,
 	dataset			INT			NOT NULL REFERENCES datasets(id),
 	website			INT			REFERENCES websites(id),
 	protocol		INT			NOT NULL,
