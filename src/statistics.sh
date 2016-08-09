1c25c5119e8a50391bc34896a55d2419907fde0d
Yann Privé <encrypt@encrypt-tips.tk>
2016-08-09 16:28:52 +0100

    Added statistics: network usage percentage

--- src/statistics.sh
+++ src/statistics.sh
@@ -40,41 +40,55 @@ process_stats() {
 	) q;
 	SQL
 	
-	info ' ├─ Getting the number of incoming flows...' '20'
+	info ' ├─ Getting the number of incoming flows...' '19'
 	
 	# Number of flows in
 	exec_sql <<- SQL
 	UPDATE stats s
 	SET flows_in = q.flows_in
 	FROM (
-		SELECT timestamp::DATE AS day,
-			count(*) AS flows_in
-		FROM flows f
-		WHERE f.endpoint_b = '${arg_ip}'
-		GROUP BY day
+		SELECT s.day,
+			coalesce(s1.flows_in, 0) AS flows_in
+		FROM stats s
+		LEFT JOIN (
+			SELECT timestamp::DATE AS day,
+				count(*) AS flows_in
+			FROM flows f
+			WHERE f.endpoint_b = '${arg_ip}'
+			GROUP BY day
+		) s1
+		ON s.day = s1.day
+		WHERE ip = '${arg_ip}'
 	) q
 	WHERE s.ip = '${arg_ip}'
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the number of outgoing flows...' '30'
+	info ' ├─ Getting the number of outgoing flows...' '28'
 	
 	# Number of flows out
 	exec_sql <<- SQL
 	UPDATE stats s
 	SET flows_out = q.flows_out
 	FROM (
-		SELECT timestamp::DATE AS day,
-			count(*) AS flows_out
-		FROM flows f
-		WHERE f.endpoint_a = '${arg_ip}'
-		GROUP BY day
+		SELECT s.day,
+			coalesce(s1.flows_out, 0) AS flows_out
+		FROM stats s
+		LEFT JOIN (
+			SELECT timestamp::DATE AS day,
+				count(*) AS flows_out
+			FROM flows f
+			WHERE f.endpoint_a = '${arg_ip}'
+			GROUP BY day
+		) s1
+		ON s.day = s1.day
+		WHERE ip = '${arg_ip}'
 	) q
 	WHERE s.ip = '${arg_ip}'
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the number of flows per 30 minutes...' '40'
+	info ' ├─ Getting the number of flows per 30 minutes...' '37'
 	
 	# Number of flows per 30 minutes (in and out)
 	exec_sql <<- SQL
@@ -112,7 +126,7 @@ process_stats() {
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the number unique websites visited...' '50'
+	info ' ├─ Getting the number unique websites visited...' '46'
 	
 	# Number of unique websites visited (which are not CDNs or ads)
 	exec_sql <<- SQL
@@ -141,7 +155,7 @@ process_stats() {
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the number of packets / payload size...' '60'
+	info ' ├─ Getting the number of packets / payload size...' '55'
 	
 	# Number of packets / payload size
 	exec_sql <<- SQL
@@ -178,7 +192,7 @@ process_stats() {
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the browsing probabilities...' '70'
+	info ' ├─ Getting the browsing probabilities...' '64'
 	
 	# Browsing probabilities
 	exec_sql <<- SQL
@@ -258,7 +272,7 @@ process_stats() {
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the number of sessions & session time...' '80'
+	info ' ├─ Getting the number of sessions & session time...' '73'
 	
 	# Browsing sessions & session time
 	exec_sql <<- SQL
@@ -329,7 +343,7 @@ process_stats() {
 		AND s.day = q.day;
 	SQL
 	
-	info ' ├─ Getting the network activity time...' '90'
+	info ' ├─ Getting the network activity time...' '82'
 	
 	# Network activity time
 	exec_sql <<- SQL
@@ -339,27 +353,23 @@ process_stats() {
 		SELECT q6.day,
 			sum(q6.duration) AS network_time
 		FROM (
-			SELECT s2.day, CASE
+			SELECT s1.day, CASE
 				WHEN q5.day_start = q5.day_end
 					THEN q5.timestamp_end - q5.timestamp_start
 				ELSE CASE
-					WHEN q5.day_start = s2.day
+					WHEN q5.day_start = s1.day
 						THEN q5.day_start + INTERVAL '24 hours' - q5.timestamp_start
-					WHEN q5.day_end = s2.day
-						THEN q5.timestamp_end - s2.day
+					WHEN q5.day_end = s1.day
+						THEN q5.timestamp_end - s1.day
 					ELSE
 						INTERVAL '24 hours'
 					END
 				END AS duration
 			FROM (
-				SELECT generate_series(day_start, day_end, '1 day'::INTERVAL)::DATE AS day
-				FROM (
-					SELECT min(day) AS day_start,
-						max(day) AS day_end
-					FROM stats
-					WHERE ip = '${arg_ip}'
-				) s1
-			) s2
+				SELECT day
+				FROM stats
+				WHERE ip = '${arg_ip}'
+			) s1
 			LEFT JOIN (
 				SELECT min(q4.start_time) AS timestamp_start,
 					max(q4.end_time) AS timestamp_end,
@@ -388,9 +398,47 @@ process_stats() {
 				) q4
 				GROUP BY q4.left_edge
 			) q5
-			ON s2.day BETWEEN q5.day_start AND q5.day_end
+			ON s1.day BETWEEN q5.day_start AND q5.day_end
 		) q6
 		GROUP BY q6.day
+	) q
+	WHERE s.ip = '${arg_ip}'
+		AND s.day = q.day;
+	SQL
+	
+	info ' ├─ Getting the network usage percentages...' '91'
+	
+	# Network usage percentages
+	exec_sql <<- SQL
+	UPDATE stats s
+	SET percent_flows = q.percent_flows,
+		percent_packets = q.percent_packets,
+		percent_payload = q.percent_payload
+	FROM (
+		SELECT day,
+			(user_flows / total_flows * 100) AS percent_flows,
+			(user_packets / total_packets * 100) AS percent_packets,
+			(user_payload / total_payload * 100) AS percent_payload
+		FROM (
+			SELECT s.day,
+				q1.total_flows::FLOAT,
+				(s.flows_in + s.flows_out)::FLOAT AS user_flows,
+				q1.total_packets::FLOAT,
+				(s.packets_in + s.packets_out)::FLOAT AS user_packets,
+				q1.total_payload::FLOAT,
+				(s.payload_size_in + s.payload_size_out)::FLOAT AS user_payload
+			FROM stats s
+			JOIN (
+				SELECT timestamp::DATE AS day,
+					count(*) AS total_flows,
+					sum(payload_size_ab + payload_size_ba) AS total_payload,
+					sum(packets_nb_ab + packets_nb_ba) AS total_packets
+				FROM flows
+				GROUP BY day
+			) q1
+			ON s.day = q1.day
+			WHERE s.ip = '${arg_ip}'
+		) q2
 	) q
 	WHERE s.ip = '${arg_ip}'
 		AND s.day = q.day;
